c3d901aaa793d006abf99e0417bb287f298d569d
Ayush Kumar <k.ayush2702@gmail.com>
2026-01-17 18:13:44 +0530

    added second generator function for 'H'

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-use k256::{ProjectivePoint, Scalar, elliptic_curve::Field};
+use k256::{
+    EncodedPoint, ProjectivePoint, Scalar,
+    elliptic_curve::{Field, sec1::FromEncodedPoint},
+};
 use rand::rngs::OsRng;
+use sha2::{Digest, Sha256};
 
 pub mod prover;
 pub mod transcript;
@@ -8,6 +12,34 @@ pub mod verifier;
 pub fn generate_keys() -> (Scalar, ProjectivePoint) {
     let sk = Scalar::random(OsRng);
     let pk = ProjectivePoint::GENERATOR * sk;
-
     (sk, pk)
+}
+
+pub fn generate_second_generator() -> ProjectivePoint {
+    let seed_string = "Pederson_Commitment_H_";
+    let mut counter = 0u32;
+
+    loop {
+        let mut hasher = Sha256::new();
+        hasher.update(seed_string.as_bytes());
+        hasher.update(counter.to_be_bytes());
+        let hash_result = hasher.finalize();
+
+        let mut compressed_bytes = [0u8; 33];
+        compressed_bytes[0] = 0x02;
+        compressed_bytes[1..33].copy_from_slice(&hash_result);
+
+        let encoded_point = EncodedPoint::from_bytes(compressed_bytes);
+        let projective_point = ProjectivePoint::from_encoded_point(&encoded_point.unwrap());
+
+        if projective_point.is_some().into() {
+            return projective_point.unwrap();
+        }
+
+        counter += 1;
+
+        if counter > 1_000_000 {
+            panic!("Could Not Find a Valid Point on Curve");
+        }
+    }
 }
